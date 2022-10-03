4bd810967a2126f92bc44c7772e17f4213431192
Adrian <machadopereiraadrian@gmail.com>
2022-10-03 16:06:28 -0300

    Testando conexão xbee

--- Main.py
+++ Main.py
@@ -4,8 +4,11 @@ import serial
 import sys
 from digi.xbee.devices import XBeeDevice
 
-MAX_PORTAS = 32 # Número máximo de portas para verificação
+MAX_PORTAS = 32
+PORT = "COM1"
+BIT_RATE = 9600
 
+##check ports
 class Portas :
     def getPortsInUse():
         for p in ['COM%s' % (i + 1) for i in range(Portas.MAX_PORTAS)]:
@@ -15,3 +18,19 @@ class Portas :
                 yield s.name
             except (OSError, serial.SerialException) :
                 pass
+
+##opening device connection
+device = XBeeDevice(PORT, BIT_RATE)
+try:
+    device.open()
+
+    ##Get the XBee protocol
+    protocol = device.get_protocol()
+
+    ##Get the operating mode
+    operanting_mode = device._get_operating_mode()
+finally:
+    if device is not None and device.is_open()
+        device.close()
+
+
