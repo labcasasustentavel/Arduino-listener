45b5b8a087ce03f5c0b57235d0e4c1eac7d654d9
Adrian <machadopereiraadrian@gmail.com>
2022-10-06 17:45:05 -0300

    Lendo somente um dispositivo

--- Main.py
+++ Main.py
@@ -5,32 +5,11 @@ import sys
 from digi.xbee.devices import XBeeDevice
 
 MAX_PORTAS = 32
-PORT = "COM1"
+PORT = 'COM5'
 BIT_RATE = 9600
 
-##check ports
-class Portas :
-    def getPortsInUse():
-        for p in ['COM%s' % (i + 1) for i in range(Portas.MAX_PORTAS)]:
-            try: 
-                s =  serial.Serial(p)
-                s.close()
-                yield s.name
-            except (OSError, serial.SerialException) :
-                pass
+arduino = serial.Serial(PORT,BIT_RATE)
 
-##opening device connection
-device = XBeeDevice(PORT, BIT_RATE)
-try:
-    device.open()
-
-    ##Get the XBee protocol
-    protocol = device.get_protocol()
-
-    ##Get the operating mode
-    operanting_mode = device._get_operating_mode()
-finally:
-    if device is not None and device.is_open()
-        device.close()
-
-
+while True:
+    linha = str(arduino.readline())
+    print(linha)
